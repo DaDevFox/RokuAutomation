f76585ca9a407de40716cd9af7b8b3bd53f43b3e
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-05-26 17:50:15 -0400

    finished app search nt, yt, prime

---
+++ commands.py
@@ -0,0 +1,129 @@
+from roku import Roku; 
+import requests; 
+import time; 
+
+roku: Roku = None
+continuing = False
+
+def roku_init(ip):
+    global roku
+    roku=Roku(ip, '8060'); 
+    roku.poweron()
+
+def app_roku_init(ip, app):
+    global roku
+    roku = Roku(ip, '8060')
+    continuing = roku.active_app.name == app
+    roku.poweron()
+    (roku[app]).launch()
+
+def app_search(ip, app, query):
+    app_roku_init(ip, app)
+    if(continuing):
+        print("running from inside app; assuming preparation for text input complete")
+
+    select = False
+
+    if(app =='Netflix'): 
+        time.sleep(2)
+        if not continuing:
+            time.sleep(6)
+            roku.select()
+            time.sleep(1.0)
+            roku.left()
+            time.sleep(0.2)
+            roku.up()
+            time.sleep(0.2)
+            roku.select()
+            time.sleep(0.2)
+        roku.literal(query)
+        time.sleep(0.5 * len(query));
+        [roku.right() for _ in range(6 - (ord(query[len(query) - 1]) - ord('a')) % 6)]
+        if select:
+            roku.select()
+    elif(app =='YouTube'): 
+        time.sleep(20)
+        roku.back() # for occasional startup modal
+        time.sleep(2)
+        roku.left()
+        time.sleep(1)
+        roku.up()
+        time.sleep(0.5)
+        roku.select()
+        time.sleep(2)
+        roku.right() 
+        roku.literal(query)
+        time.sleep(0.2 * len(query))
+        roku.left()
+        [roku.down() for _ in range(5)]
+        roku.right() # in case of sponsored/ad videos
+        if select:
+            roku.select()
+    if(app =='Prime Video'): 
+        time.sleep(6 if continuing else 20)
+        roku.select()
+        time.sleep(6.0)
+        roku.down()
+        time.sleep(0.8)
+        roku.left()
+        time.sleep(0.8)
+        roku.up()
+        time.sleep(0.8)
+        roku.select()
+        time.sleep(2)
+
+        curr_x = 0
+        curr_y = 0
+        move_delay = 0.01
+        result_delay = 2
+        print(f"typing {query.lower()}")
+        for character in query.lower():
+            target_x = (ord(character) - ord('a') ) % 6
+            target_y = (ord(character) - ord('a')) // 6
+            if character == ' ':
+                target_x = 0
+                target_y = 6
+            if character.isdecimal():
+                # TODO
+                pass
+
+            diff_x = target_x - curr_x
+            diff_y = target_y - curr_y
+
+            print(f"target ({target_x}, {target_y}), diff ({diff_x}, {diff_y})")
+
+            for _ in range(abs(diff_x)):
+                if diff_x > 0:
+                    roku.right()
+                    time.sleep(move_delay)
+                else:
+                    roku.left()
+                    time.sleep(move_delay)
+            
+            for _ in range(abs(diff_y)):
+                if diff_y > 0:
+                    roku.down()
+                    time.sleep(move_delay)
+                else:
+                    roku.up()
+                    time.sleep(move_delay)
+            
+            print(character, end=' ')
+            roku.select()
+
+            curr_x = target_x
+            curr_y = target_y
+
+            if character == ' ':
+                roku.up()
+                time.sleep(move_delay)
+                curr_y -= 1
+        print('\n')
+
+        [roku.up() for _ in range(curr_y)]
+        [roku.right() for _ in range( 6 - curr_x)]
+        time.sleep(result_delay)
+        roku.right()
+        if select:
+            roku.select()
+
