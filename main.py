a82b8d680eb51f5cd21a3443ecdabf029ba2bf22
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-05-26 18:19:47 -0400

    fix potential issue on other machines

--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import subprocess
 import os
 import commands
 
-n = len(sys.argv)
+argv = sys.argv
+if argv[0] == 'python':
+    argv.pop(0)
+
+n = len(argv)
 
 if n < 3:
     print("requires a command to be run and a provided device IP address\n")
     sys.exit(1)
 
-function = getattr(commands, sys.argv[1])
+
+function = getattr(commands, argv[1])
 if not function: 
     print(f"not a valid command\n")
     sys.exit(1)
 else:
-    function(*sys.argv[2:])
+    function(*argv[2:])
