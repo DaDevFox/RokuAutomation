2b5e650adf642554af4ffc9f8c7f56006170e8ea
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-05-26 17:50:00 -0400

    change command controller system

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 import sys
 import subprocess
 import os
+import commands
 
-commands_dir = 'Commands'
-commands = {}
-
-for subdir, dirs, files in os.walk(os.path.join(os.path.dirname(os.path.realpath(__file__)), commands_dir)):
-   for file in files:
-      if file[0] == '_' :
-        commands[(file.split('.')[0])[1:]] = file; 
-         
 n = len(sys.argv)
 
-if n < 2:
-    print("requires a command to be run")
-
-if not commands.__contains__(sys.argv[0]):
-    print(f"commands available: \n{commands}")
+if n < 3:
+    print("requires a command to be run and a provided device IP address\n")
     sys.exit(1)
 
-subprocess.call(sys.argv)
-
-
-
-
-
+function = getattr(commands, sys.argv[1])
+if not function: 
+    print(f"not a valid command\n")
+    sys.exit(1)
+else:
+    function(*sys.argv[2:])
